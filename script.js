e8f1f77f3adb963f3cbf6d10bf7be2fcf58ffe43
pushpamohan-abhi <pushpamohan.cs@hkbk.edu.in>
2026-03-14 09:48:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,58 @@ async function getResult(){
 
 let usn=document.getElementById("usn").value;
 
-let res = await fetch("/.netlify/functions/result?usn="+usn);
+let res=await fetch("/.netlify/functions/result?usn="+usn);
 
-let data = await res.json();
+let data=await res.json();
 
-document.getElementById("result").innerHTML =
+document.getElementById("result").innerHTML=
 "Name: "+data.name+
 "<br>SGPA: "+data.sgpa;
 
 }
 
 
+
 async function getAnalysis(){
 
-let res = await fetch("/.netlify/functions/analysis");
+let res=await fetch("/.netlify/functions/analysis");
 
-let data = await res.json();
+let data=await res.json();
 
-document.getElementById("analysis").innerHTML =
+document.getElementById("analysis").innerHTML=
+
 "Pass Percentage: "+data.passPercentage+
 "<br>Average SGPA: "+data.averageSGPA+
-"<br>Topper: "+data.topper.usn;
+"<br>Topper: "+data.topper;
+
+createChart(data);
+
+}
+
+
+
+function createChart(data){
+
+new Chart(document.getElementById("chart"),{
+
+type:"bar",
+
+data:{
+
+labels:["Pass %","Average SGPA"],
+
+datasets:[{
+
+label:"CSE Performance",
+
+data:[data.passPercentage,data.averageSGPA],
+
+backgroundColor:["green","blue"]
+
+}]
+
+}
+
+});
 
 }
