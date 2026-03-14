b56ea9078f578dabdaa36cc3b2c3fbc5bb941768
pushpamohan-abhi <pushpamohan.cs@hkbk.edu.in>
2026-03-14 09:45:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+async function getResult(){
+
+let usn=document.getElementById("usn").value;
+
+let res = await fetch("/.netlify/functions/result?usn="+usn);
+
+let data = await res.json();
+
+document.getElementById("result").innerHTML =
+"Name: "+data.name+
+"<br>SGPA: "+data.sgpa;
+
+}
+
+
 async function getAnalysis(){
 
 let res = await fetch("/.netlify/functions/analysis");
@@ -5,7 +20,7 @@ let res = await fetch("/.netlify/functions/analysis");
 let data = await res.json();
 
 document.getElementById("analysis").innerHTML =
-"Pass %: "+data.passPercentage+
+"Pass Percentage: "+data.passPercentage+
 "<br>Average SGPA: "+data.averageSGPA+
 "<br>Topper: "+data.topper.usn;
 
