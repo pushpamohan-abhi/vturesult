f6e2526cf3edd330cdb33db59e35c2b4223aaf25
pushpamohan-abhi <pushpamohan.cs@hkbk.edu.in>
2026-03-14 09:38:00 +0530

    Update result.js

--- netlify/functions/result.js
+++ netlify/functions/result.js
@@ -1,12 +1,35 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
 exports.handler = async (event) => {
 
-const usn = event.queryStringParameters.usn;
+let usn = event.queryStringParameters.usn;
+
+let url = "https://results.vtu.ac.in/result?usn=" + usn;
+
+let response = await axios.get(url);
+
+let $ = cheerio.load(response.data);
+
+let name = $("#studentName").text();
+let sgpa = $("#sgpa").text();
+
+let subjects = [];
+
+$("table tr").each(function(){
+   let subject = $(this).find("td:eq(0)").text();
+   let result = $(this).find("td:eq(4)").text();
+
+   subjects.push({subject,result});
+});
 
 return {
-statusCode: 200,
-body: JSON.stringify({
-name:"Sample Student",
-sgpa:"8.2"
+statusCode:200,
+body:JSON.stringify({
+usn,
+name,
+sgpa,
+subjects
 })
 };
 
